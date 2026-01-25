44cf1e7377465cf26c872ed8534c7ed09a8406ea
Lail <pageoflail@gmail.com>
2026-01-25 19:19:14 +0700

    ubah posisi card -81

--- app/next/page.tsx
+++ app/next/page.tsx
@@ -159,7 +159,7 @@ export default function ScratchPage() {
   <div
     style={{
       position: 'absolute',
-      bottom: -83, // 🔥 INI KUNCINYA
+      bottom: -81, // 🔥 INI KUNCINYA
       left: 12,
       right: 12,
       background: '#fff',
